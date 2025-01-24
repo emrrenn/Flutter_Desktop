65e3ac29403d73da669665ca87d59968371c92ed
emrrenn <emreren1224@gmail.com>
2025-01-24 16:38:35 +0300

    Upgraded the main page

--- lib/main.dart
+++ lib/main.dart
@@ -16,9 +16,90 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: const Text("Unit Information"),
         ),
-        body: const Center(
-          child: Text('Welcome to Unit Information App!'),
-        ),
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text(
+                'Begin by entering the width, length, height and weight of each unit below',
+                style: TextStyle(fontSize: 16.0),
+              ),
+              const SizedBox(height: 20),
+              const Text(
+                'Unit Packaging',
+                style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {}, 
+                    child: const Text('Box'),
+                  ),
+                  const SizedBox(width: 10),
+                  ElevatedButton(
+                    onPressed: () {}, 
+                    child: const Text('Bag')
+                  ),
+                ],
+              ),
+              const SizedBox(height: 20),
+              const Text(
+                'Unit Dimensions',
+                style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 20),
+              Row(
+                children: [
+                  Flexible(
+                    child: TextField(
+                      decoration: const InputDecoration(
+                        labelText: 'Width',
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(width: 10),
+                  Flexible(
+                    child: TextField(
+                      decoration: const InputDecoration(
+                        labelText: 'Height',
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 20),
+              const Text(
+                'Unit Weight',
+                style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 10),
+              TextField(
+                decoration: const InputDecoration(
+                  labelText: 'Weight',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              const SizedBox(height: 30),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {}, 
+                    child: const Text('Previous Step'),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {}, 
+                    child: const Text('Next Step'),
+                  ),
+                ],
+              )
+            ],
+          ),
+        )
       ),
     );
   }
